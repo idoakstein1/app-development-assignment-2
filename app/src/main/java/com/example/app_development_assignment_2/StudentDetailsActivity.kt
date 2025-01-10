2d69b6a360fc849184a295cda51302781fb7e0be
guyattias <guyattias23@gmail.com>
2025-01-10 14:27:54 +0200

    Finish edit student details

--- app/src/main/java/com/example/app_development_assignment_2/StudentDetailsActivity.kt
+++ app/src/main/java/com/example/app_development_assignment_2/StudentDetailsActivity.kt
@@ -1,14 +1,15 @@
 package com.example.app_development_assignment_2
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.app_development_assignment_2.model.Student
+import com.example.app_development_assignment_2.model.Model
 
 
 class StudentDetailsActivity : AppCompatActivity() {
@@ -33,7 +34,27 @@ class StudentDetailsActivity : AppCompatActivity() {
         val studentCheckedCheckBox =
             findViewById<CheckBox>(R.id.student_details_activity_checked_check_box)
 
-        val student = intent.getSerializableExtra("student") as? Student
+        val editStudentDetailsButton = findViewById<Button>(R.id.student_details_activity_edit_details_button)
+        val cancelButton = findViewById<Button>(R.id.student_details_activity_cancel_button)
+
+        val studentPosition = intent.getSerializableExtra("studentPosition") as Int
+        val student = Model.shared.students[studentPosition]
+
+        editStudentDetailsButton.setOnClickListener(
+            {
+                val intent = Intent(this, EditStudentDetails::class.java).apply {
+                    putExtra("studentPosition", studentPosition)
+                }
+                startActivity(intent)
+            }
+        )
+
+        cancelButton.setOnClickListener(
+            {
+                finish()
+            }
+        )
+
 
         student?.let {
 
--- app/src/main/java/com/example/app_development_assignment_2/model/Student.kt
+++ app/src/main/java/com/example/app_development_assignment_2/model/Student.kt
@@ -1,11 +1,9 @@
 package com.example.app_development_assignment_2.model
 
-import java.io.Serializable
-
 data class Student (
     val name: String,
     val id: String,
     val phone: String,
     val address: String,
     var isChecked: Boolean = false
-):Serializable
+)
